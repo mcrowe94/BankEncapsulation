3ea9da8d7e4ed2cf9e4bc96710fcdac8c43a3230
Monica Crowe <mcrowe9194@gmail.com>
2022-05-10 20:10:10 -0500

    Add project files.

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! your balance is now {account.GetBalance()}!"); 
+
+                
+        }
+    }
+}
